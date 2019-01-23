5c98a856bfeaf1eff485768c7e930114045b6a3a
Ellis Rhys Thomas <e.rhys.thomas@gmail.com>
2019-01-23 22:19:38 +0000

    methods for battery level, state as well as reading the time

--- src/drn_cb.c
+++ src/drn_cb.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "ert_log.h"
 
 #define MAXSTR 200+1
 #define MAXPATH 2000+1
 
+#define BAT_CAPACITY "/sys/class/power_supply/BAT0/capacity"
+#define BAT_STATUS "/sys/class/power_supply/BAT0/status"
+
 char *
 create_buf(size_t size)
 {
-    char *buf = calloc(1, sizeof size);
+    char *buf = calloc(1, size);
     if (!buf) {
 	log_err("Memory Error");
 	buf = NULL;
@@ -17,84 +21,109 @@ create_buf(size_t size)
     return buf;
 }
     
+/* Reads a line from stream into str_out, recording the number of
+   characters into len_out */
+int
+read_line(FILE *stream, char **str_out, size_t *len_out)
+{
+    if (!fgets(*str_out, *len_out, stream)) {
+	log_err("Failed to read file");
+	return 1;
+    }
+
+    /* remove newline character if present */
+    *len_out = strnlen(*str_out, *len_out);
+    if (((*str_out)[*len_out - 1]) == '\n') {
+	(*str_out)[*len_out - 1] = '\0';
+	--(*len_out);
+    }
+
+    log_debug("String: %s, Length: %zu", *str_out, *len_out);
+
+    return 0;
+}
 
 int
-sysfs_read(const char *pathname, char **sys_out)
+read_sysfs(const char *pathname, char **sys_out, size_t *len_out)
 {
- /*    char *pathname = create_buf(MAXPATH); */
- /*    if (!pathname) goto err1; */
-    
- /*    FILE *sysfile = fopen(pathname, "r"); */
- /*    if (!sysfile) { */
- /* 	log_err("Failed to open sysfile %s", pathname); */
- /* 	goto err2; */
- /*    } */
-
- /*    if ( !fgets(*sys_out, MAXSTR, sysfile) ) { */
- /* 	log_err("Failed to read sysfile %s", pathname); */
- /* 	goto err3; */
- /*    } */
-
- /*    int rc = fclose(sysfile); */
- /*    if (!rc) log_warn("Failed to close sysfile"); */
-
- /*    //free(S */
-
- /*    return 0; */
-
- /* err3: */
- /*    fclose(sysfile); */
- /* err2: */
- /*    free(str); */
- /* err1: */
- /*    return 1; */
-}
-
-/* Returns capacity from SYS_BAT0/capacity */
-char *
-drn_test1(void)
-{
-    char *str = create_buf(MAXSTR);
-    if (!str) {
-	str = NULL;
-	goto out;
+    if (!pathname || !sys_out || !(*sys_out)) {
+	log_err("Invalid arguments");
+	return 1;
     }
-    
-    strcpy(str, "God");
 
- out:
-    return str;
+    FILE *sysfile = fopen(pathname, "r");
+    if (!sysfile) {
+	log_err("Failed to open %s", pathname);
+	return 1;
+    }
+
+    if (read_line(sysfile, sys_out, len_out)) {
+	log_err("Failed to read %s", pathname);
+	return 1;
+    }
+
+    if (fclose(sysfile))
+	log_warn("Failed to close %s", pathname);
+
+    return 0;
+}
+
+/* Battery capacity */
+char *
+batt_capacity()
+{
+    size_t capl = MAXSTR;
+    char *capacity = create_buf(capl);
+    if (!capacity) return NULL;
+    if (read_sysfs(BAT_CAPACITY, &capacity, &capl)) return NULL;
+    
+    const char *units = "%";
+
+    if ((MAXSTR - capl) < strlen(units)) {
+	log_warn("Buffer too full to append units");	
+	return capacity;
+    }
+
+    return strncat(capacity, units, MAXSTR);
+}
+
+/* Battery status (charging or discharging) */
+char *
+batt_status()
+{
+    size_t statl = MAXSTR;
+    char *status = create_buf(statl);
+    if (!status) return NULL;
+    if (read_sysfs(BAT_STATUS, &status, &statl)) return NULL;
+
+    return status;
 }
 
 char *
-drn_test2(void)
+local_time()
 {
-    char *str = create_buf(MAXSTR);
-    if (!str) {
-	str = NULL;
+    char *disptime = create_buf(MAXSTR);
+    if (!disptime) return NULL;
+
+    /* simple time is the number of seconds elapsed since 00:00:00 on
+       January 1, 1970, Coordinated Universal Time */
+    const time_t simpletime = time(NULL);
+    if ((time_t)(-1) == simpletime) goto out;
+
+    /* broken down time is a binary representation of calendar time
+       separated into year, month, day, and so on */
+    struct tm *brokendowntime = localtime(&simpletime);
+    if (!brokendowntime) goto out;
+
+    if (!(strftime(disptime, MAXSTR, "%a %d/%m/%y %I:%M%p", brokendowntime)))
 	goto out;
-    }
-    
-    strcpy(str, "Ma");
 
+    return disptime;
  out:
-    return str;
+    log_err("Cannot determine time");
+    free(disptime);
+    return NULL;
 }
-char *
-drn_test3(void)
-{
-    char *str = create_buf(MAXSTR);
-    if (!str) {
-	str = NULL;
-	goto out;
-    }
-    
-    strcpy(str, "Ohhhhh");
-
- out:
-    return str;
-}
-
     
 
 
